650043f7e3dd1f256286fcf1e46af8aaa3d3c3b4
Jason Chen <superj63@hotmail.com>
2019-09-03 23:56:09 -0700

    finish selection

--- algos/sorting_algos.py
+++ algos/sorting_algos.py
@@ -6,7 +6,7 @@ def swap(arr, i, j):
 
 # Bubble sort: Worst Case = Best Case = O(n^2)
 def bubble(arr, ascending=True):
-  for i in range(0, len(arr)-1):
+  for i in range(0, len(arr)):
     for j in range(0, len(arr) - i - 1):
       if (ascending == True):
         if (arr[j] > arr[j + 1]):
@@ -18,6 +18,7 @@ def bubble(arr, ascending=True):
   print(arr)
   return arr
 
+# Selection sort: Worst Case = Best Case = O(n^2)
 def selection(arr, ascending=True):
   for i in range(0, len(arr)):
     min = i
@@ -34,9 +35,12 @@ def selection(arr, ascending=True):
   print(arr)
   return arr
 
+# Insertion sort
+def insertion(arr, )
+
 def main():
   arr = [4,2,3,1,5]
-  selection(arr, False)
+  bubble(arr, False)
 
 if __name__ == "__main__":
   main()
