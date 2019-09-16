d97bdf978005921b91b10a6d839a86cc5a6bd441
Jason Chen <superj63@hotmail.com>
2019-09-15 22:24:39 -0700

    Working version

--- visualizer.py
+++ visualizer.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 import sorting_algos as srt
+from matplotlib.collections import PatchCollection
 
 plt.style.use('seaborn-pastel')
 
 def main():
   # Initialization
-  arr = srt.arrgen(lo=0, hi=10, s=10)
+  arr = srt.arrgen(lo=0, hi=50, s=50)
   temp = srt.copy.deepcopy(arr)
   a, frames = srt.bubble(temp, ascending=True)
 
@@ -14,19 +15,17 @@ def main():
   fig, ax = plt.subplots(figsize=(6, 6))
   ax.set_title('Sorting Visualizer')
   ax.set_xlabel('Values')
-  rects = ax.bar(range(len(arr)),arr)
+  rects = ax.bar(range(len(arr)), arr)
 
   # Animate
-  def animate(i):
-    print(i)
-    for f in i:
-      print(f)
-      for rect, y in zip(rects, f):
-        rect.set_height(y)
+  def animate(fi):
+    for rect, y in zip(rects, frames[fi]):
+      rect.set_height(y)
+    return (rects)
+
   # Show animation
-  anim = FuncAnimation(fig,animate,frames=len(frames),interval=200, blit=False)
+  anim = animation.FuncAnimation(fig,animate,frames=len(frames),interval=len(frames),repeat=False, blit=False)
   plt.show()
-  pass
 
 if __name__ == "__main__":
   main()
